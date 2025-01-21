4007746c15853ec8549825f08baee30d8992383a
wawanpi <muhaguskurniawan@gmail.com>
2025-01-22 00:55:32 +0700

    Memanngil PengelolaHalaman di main activiti

--- app/src/main/java/com/example/klinikhewan/MainActivity.kt
+++ app/src/main/java/com/example/klinikhewan/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.klinikhewan.ui.theme.KlinikHewanTheme
+import com.example.pertemuan12.ui.navigation.PengelolaHalaman
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             KlinikHewanTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
